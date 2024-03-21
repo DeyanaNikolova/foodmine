c1d4e39d23b7c05afcc7e88ea599f49937f32e38
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-21 16:36:10 +0000

    Add cart functionality to food page

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Food } from '../../../shared/models/food';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FoodService } from '../../../services/food.service';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-food-page',
@@ -13,12 +14,19 @@ export class FoodPageComponent {
 
   constructor(
     activatedRoute: ActivatedRoute,
-    private foodService: FoodService
+    foodService: FoodService,
+    private cartService: CartService,
+    private router: Router
   ) {
     activatedRoute.params.subscribe((params) => {
       if (params.id) {
-        this.food = this.foodService.getFoodById(params.id);
+        this.food = foodService.getFoodById(params.id);
       }
     });
+  }
+
+  addToCart(): void{
+this.cartService.addToCart(this.food);
+this.router.navigateByUrl('/cart-page');
   }
 }
