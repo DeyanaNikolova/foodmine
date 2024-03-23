6e5661a118056ad35513faac2a965b371ac3427a
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-23 19:23:11 +0000

    Remove OnInit

--- frontend/src/app/components/partials/tags/tags.component.ts
+++ frontend/src/app/components/partials/tags/tags.component.ts
@@ -1,21 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FoodService } from '../../../services/food.service';
 import { Tag } from '../../../shared/models/Tag';
 
 @Component({
   selector: 'app-tags',
   templateUrl: './tags.component.html',
-  styleUrl: './tags.component.css'
+  styleUrl: './tags.component.css',
 })
-export class TagsComponent implements OnInit{
- tags: Tag[] = [];
+export class TagsComponent {
+  tags: Tag[] = [];
 
-constructor(foodService: FoodService) {
-  foodService.getAllTags().subscribe(serverTags=>{
-    this.tags = serverTags;
-  });
-}
-
-ngOnInit(): void {
-}
+  constructor(foodService: FoodService) {
+    foodService.getAllTags().subscribe((serverTags) => {
+      this.tags = serverTags;
+    });
+  }
 }
