06101342e084f9177f4efcb93f1a8a39555638cd
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-23 19:20:07 +0000

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import { sample_foods, sample_tags } from "./data";
 
 const app = express();
+// app.use(express.json());
 //localhost:4200 -> frontend
 //localhost:5000 -> backend
 app.use(
@@ -14,36 +15,35 @@ app.use(
 
 app.get("/api/foods", (req, res) => {
   res.send(sample_foods);
-});
+})
 
 app.get("/api/foods/search/:searchTerm", (req, res) => {
   const searchTerm = req.params.searchTerm;
-  const foods = sample_foods
-  .filter(food => food.name.toLowerCase()
-  .includes(searchTerm.toLowerCase()));
+  const foods = sample_foods.filter((food) =>
+    food.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   res.send(foods);
-});
+})
 
-app.get("/api/foods/tags", (req, res)=>{
-    res.send(sample_tags);
-});
+app.get("/api/foods/tags", (req, res) => {
+  res.send(sample_tags); 
+})
 
-app.get("/api/foods/tag/:tagName", (req, res)=>{
-    const tagName = req.params.tagName;
-    const food = sample_foods.filter(food => food.tags?.includes(tagName));
-    res.send(food);
-});
+app.get("/api/foods/tag/:tagName", (req, res) => {
+  const tagName = req.params.tagName;
+  const food = sample_foods.filter((food) => food.tags?.includes(tagName));
+  res.send(food);
+})
 
-app.get("/api/foods/:foodId", (req, res)=>{
-    const foodId = req.params.foodId;
-    const food = sample_foods.find((food) => food.id === foodId);
-    res.send(food);
-});
-
+app.get("/api/foods/:foodId", (req, res) => {
+  const foodId = req.params.foodId;
+  const food = sample_foods.find((food) => food.id === foodId);
+  res.send(food);
+})
 
 
 
 const port = 5000;
 app.listen(port, () => {
   console.log("Website served on http://localhost:" + port);
-});
+})
