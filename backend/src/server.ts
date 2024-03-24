f657310c32cf1c33a73f431426ed2017dcae5ec4
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-24 12:42:01 +0000

    Add user router to server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,7 @@
 import express from "express";
 import cors from "cors";
-import { sample_foods, sample_tags, sample_users } from "./data";
-import jwt from "jsonwebtoken";
 import foodRouter from './routers/food.router';
+import userRouter from './routers/user.router';
 const app = express();
 app.use(express.json());
 //localhost:4200 -> frontend
@@ -15,34 +14,8 @@ app.use(
 );
 
 app.use("/api/foods", foodRouter);
+app.use('/api/users', userRouter);
 
-app.post("/api/users/login", (req, res) => {
-  const { email, password } = req.body;
-  const user = sample_users.find(
-    (user) => user.email === email && user.password === password
-  );
-  if (user) {
-    res.send(generateTokenResponse(user));
-  }else{
-    res.status(400).send("User name or password is not valid!");
-  }
-});
-
-const generateTokenResponse = (user: any) => {
-  const token = jwt.sign(
-    {
-      email: user.email,
-      isAdmin: user.isAdmin,
-    },
-    "SomeRandomText",
-    {
-      expiresIn: "30d",
-    }
-  );
-
-  user.token = token;
-  return user;
-};
 
 const port = 5000;
 app.listen(port, () => {
