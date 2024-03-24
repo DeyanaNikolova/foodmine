6695818b9d6b77d46255eda8308ee9f5fa5f3bfa
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-24 14:26:51 +0000

    Seed foods

--- backend/src/routers/food.router.ts
+++ backend/src/routers/food.router.ts
@@ -1,35 +1,49 @@
-import { Router } from 'express';
-import { sample_foods, sample_tags } from '../data';
+import { Router } from "express";
+import { sample_foods, sample_tags } from "../data";
+import asyncHandler from "express-async-handler";
+import { FoodModel } from "../models/food.model";
 
-const router = Router(); 
+const router = Router();
 
+router.get(
+  "/seed",
+  asyncHandler(async (req, res) => {
+    const foodCount = await FoodModel.countDocuments();
+    if (foodCount > 0) {
+      res.send("Seed is already done!");
+      return;
+    }
+    await FoodModel.create(sample_foods);
+    res.send("Seed is done!");
+  })
+);
 
 router.get("/", (req, res) => {
-    res.send(sample_foods);
-  });
-  
-  router.get("/search/:searchTerm", (req, res) => {
-    const searchTerm = req.params.searchTerm;
-    const foods = sample_foods.filter((food) =>
-      food.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    res.send(foods);
-  });
-  
-  router.get("/tags", (req, res) => {
-    res.send(sample_tags);
-  });
-  
-  router.get("/tag/:tagName", (req, res) => {
-    const tagName = req.params.tagName;
-    const food = sample_foods.filter((food) => food.tags?.includes(tagName));
-    res.send(food);
-  });
-  
-  router.get("/:foodId", (req, res) => {
-    const foodId = req.params.foodId;
-    const food = sample_foods.find((food) => food.id === foodId);
-    res.send(food);
-  });
+  res.send(sample_foods);
+});
 
-  export default router;
+router.get("/search/:searchTerm", (req, res) => {
+  const searchTerm = req.params.searchTerm;
+  const foods = sample_foods.filter((food) =>
+    food.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  res.send(foods);
+});
+
+router.get("/tags", (req, res) => {
+  res.send(sample_tags);
+});
+
+router.get("/tag/:tagName", (req, res) => {
+  const tagName = req.params.tagName;
+  const food = sample_foods.filter((food) => food.tags?.includes(tagName));
+  res.send(food);
+});
+
+router.get("/:foodId", (req, res) => {
+  const foodId = req.params.foodId;
+  const food = sample_foods.find((food) => food.id === foodId);
+  res.send(food);
+});
+
+export default router;
