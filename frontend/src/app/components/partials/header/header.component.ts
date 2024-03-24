ef6c754dc4b72d48620baee3a1cf96ad88096060
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-24 09:02:52 +0000

    Subscribe to user

--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { CartService } from '../../../services/cart.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/shared/models/User';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-cartQuantity = 0;
+  cartQuantity = 0;
+  user!: User;
 
+  constructor(cartService: CartService, private userService: UserService) {
+    cartService.getCartObservable().subscribe((newCart) => {
+      this.cartQuantity = newCart.totalCount;
+    });
 
-constructor(cartService: CartService) {
-  cartService.getCartObservable().subscribe((newCart)=>{
-    this.cartQuantity = newCart.totalCount;
-  })
-}
+    userService.userObservable$.subscribe((newUser)=>{
+      this.user = newUser;
+    })
+  }
 }
