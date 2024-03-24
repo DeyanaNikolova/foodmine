738c994c4ee3a768ddb151d781ac08864b3e43fc
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-24 17:12:43 +0000

    Connect user login to database

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import jwt from "jsonwebtoken";
 import { sample_users } from '../data';
 import asyncHandler from "express-async-handler";
-import { UserModel } from '../models/user.model';
+import { User, UserModel } from '../models/user.model';
 
 const router = Router();
 
@@ -19,32 +19,31 @@ router.get(
   })
 );
 
-router.post("/login", (req, res) => {
-    const { email, password } = req.body;
-    const user = sample_users.find(
-      (user) => user.email === email && user.password === password
-    );
-    if (user) {
-      res.send(generateTokenResponse(user));
-    }else{
-      res.status(400).send("User name or password is not valid!");
-    }
-  });
-  
-  const generateTokenResponse = (user: any) => {
-    const token = jwt.sign(
-      {
-        email: user.email,
-        isAdmin: user.isAdmin,
-      },
-      "SomeRandomText",
-      {
-        expiresIn: "30d",
-      }
-    );
-  
-    user.token = token;
-    return user;
-  };
+router.post("/login", asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await UserModel.findOne({email, password});
+  if (user) {
+    res.send(generateTokenResponse(user));
+  }else{
+    const BAD_REQUEST = 400;
+    res.status(BAD_REQUEST).send("Username or password is invalid!");
+  }
+}));
 
-  export default router;
+const generateTokenResponse = (user: any) => {
+  const token = jwt.sign(
+    {
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    "SomeRandomText",
+    {
+      expiresIn: "30d",
+    }
+  );
+
+  user.token = token;
+  return user;
+};
+
+export default router;
