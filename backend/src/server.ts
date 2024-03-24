8d4e4c1d47c0855f61647e1dbe076b5a59655ef5
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-24 07:16:41 +0000

    Add function for JWT and api call for login

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
-import { sample_foods, sample_tags } from "./data";
+import { sample_foods, sample_tags, sample_users } from "./data";
+import jwt from "jsonwebtoken";
 
 const app = express();
-// app.use(express.json());
+app.use(express.json());
 //localhost:4200 -> frontend
 //localhost:5000 -> backend
 app.use(
@@ -15,7 +16,7 @@ app.use(
 
 app.get("/api/foods", (req, res) => {
   res.send(sample_foods);
-})
+});
 
 app.get("/api/foods/search/:searchTerm", (req, res) => {
   const searchTerm = req.params.searchTerm;
@@ -23,27 +24,53 @@ app.get("/api/foods/search/:searchTerm", (req, res) => {
     food.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
   res.send(foods);
-})
+});
 
 app.get("/api/foods/tags", (req, res) => {
-  res.send(sample_tags); 
-})
+  res.send(sample_tags);
+});
 
 app.get("/api/foods/tag/:tagName", (req, res) => {
   const tagName = req.params.tagName;
   const food = sample_foods.filter((food) => food.tags?.includes(tagName));
   res.send(food);
-})
+});
 
 app.get("/api/foods/:foodId", (req, res) => {
   const foodId = req.params.foodId;
   const food = sample_foods.find((food) => food.id === foodId);
   res.send(food);
-})
+});
 
+app.post("/api/users/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = sample_users.find(
+    (user) => user.email === email && user.password === password
+  );
+  if (user) {
+    res.send(generateTokenResponse(user));
+  }else{
+    res.status(400).send("User name or password is not valid!");
+  }
+});
 
+const generateTokenResponse = (user: any) => {
+  const token = jwt.sign(
+    {
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    "SomeRandomText",
+    {
+      expiresIn: "30d",
+    }
+  );
+
+  user.token = token;
+  return user;
+};
 
 const port = 5000;
 app.listen(port, () => {
   console.log("Website served on http://localhost:" + port);
-})
+});
