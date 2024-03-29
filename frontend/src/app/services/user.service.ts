638fff1fcddbd52eb426b4dcc07668f1b4a9dc78
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-29 12:58:17 +0000

    Add register method

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -3,8 +3,9 @@ import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { User } from '../shared/models/User';
 import { IUserLogin } from '../shared/interfaces/IUserLogin';
 import { HttpClient } from '@angular/common/http';
-import { USER_LOGIN_URL } from '../shared/constants/urls';
+import { USER_LOGIN_URL, USER_REGISTER_URL } from '../shared/constants/urls';
 import { ToastrService } from 'ngx-toastr';
+import { IUserRegister } from '../shared/interfaces/IUserRegister';
 
 const USER_KEY = 'User';
 @Injectable({
@@ -27,8 +28,8 @@ export class UserService {
           this.setUserToLocalStorage(user);
           this.userSubject$$.next(user);
           this.toastrService.success(
-            `Welcome to Foodmine ${user.name}!`,
-            'Login Successfull'
+            `Welcome to the Foodmine ${user.name}!`,
+            'Login Successful'
           );
         },
         error: (errorResponse) => {
@@ -38,7 +39,23 @@ export class UserService {
     );
   }
 
-  register(){}
+  register(userRegister: IUserRegister): Observable<User>{
+    return this.http.post<User>(USER_REGISTER_URL, userRegister).pipe(
+      tap({
+        next: (user)=>{
+          this.setUserToLocalStorage(user);
+          this.userSubject$$.next(user);
+          this.toastrService.success(
+            `Welcome to the Foodmine ${user.name}!`,
+            'Register Successful'
+          )
+        },
+        error: (errorResponse)=>{
+          this.toastrService.error(errorResponse.error, 'Register Failed')
+        }
+      })
+    )
+  }
 
   logout(){
     this.userSubject$$.next(new User());
