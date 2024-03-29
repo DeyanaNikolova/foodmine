d9af02e87669a50f8383bf2d53f8d50dc5e9b4e0
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-29 17:19:14 +0000

    Add route to checkout page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { FoodPageComponent } from './components/pages/food-page/food-page.compon
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
-
+import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
 
 const routes: Routes = [
   {
@@ -14,28 +14,33 @@ const routes: Routes = [
   },
   {
     path: 'search/:searchTerm',
-    component: HomeComponent
+    component: HomeComponent,
   },
   {
     path: 'tag/:tag',
-    component: HomeComponent
+    component: HomeComponent,
   },
   {
     path: 'food/:id',
-    component: FoodPageComponent
+    component: FoodPageComponent,
   },
   {
     path: 'cart-page',
-    component: CartPageComponent
+    component: CartPageComponent,
   },
   {
-    path: 'login', 
-    component: LoginPageComponent
-  },{
-    path: 'register', component:RegisterPageComponent
-  }
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterPageComponent,
+  },
+  {
+    path: 'checkout',
+    component: CheckoutPageComponent,
+  },
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
