3a715a7b3ad8ef13cb7699d46c4bbda09504172e
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-30 20:17:11 +0000

    Add order pay method

--- backend/src/routers/order.router.ts
+++ backend/src/routers/order.router.ts
@@ -12,7 +12,7 @@ router.post(
   "/create",
   asyncHandler(async (req: any, res: any) => {
     const requestOrder = req.body;
-    
+
     if (requestOrder.items.length <= 0) {
       res.stattus(HTTP_BAD_REQUEST).send("Cart Is Empty!");
       return;
@@ -29,13 +29,41 @@ router.post(
   })
 );
 
-router.get('/newOrderForCurrentUser', asyncHandler(async (req:any, res)=>{
-const order = await OrderModel.findOne({user: req.user.id, status: OrderStatus.NEW});
-if(order){
-  res.send(order);
-}else{
-  res.status(HTTP_BAD_REQUEST).send();
-}
-}))
+router.get(
+  "/newOrderForCurrentUser",
+  asyncHandler(async (req: any, res) => {
+    const order = await getNewOrderForCurrentUser(req);
+    if (order) {
+      res.send(order);
+    } else {
+      res.status(HTTP_BAD_REQUEST).send();
+    }
+  })
+);
+
+router.post(
+  "/pay",
+  asyncHandler(async (req, res) => {
+    const { paymentId } = req.body;
+    const order = await getNewOrderForCurrentUser(req);
+
+    if (!order) {
+      res.status(HTTP_BAD_REQUEST).send("Order Not Found!");
+      return;
+    }
+    order.paymentId = paymentId;
+    order.status = OrderStatus.PAYED;
+    await order?.save();
+
+    res.send(order?._id);
+  })
+);
 
 export default router;
+
+async function getNewOrderForCurrentUser(req: any) {
+  return await OrderModel.findOne({
+    user: req.user.id,
+    status: OrderStatus.NEW,
+  });
+}
