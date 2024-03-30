f2cf635a29e1c54caa2b2bce6ec33b1c81c5fa7f
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-30 14:45:58 +0000

    Move auth interceptor to auth folder

---
+++ frontend/src/app/auth/auth.interceptor.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor{
+
+
+  constructor(private userService: UserService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+   const user = this.userService.currentUser;
+   if(user.token){
+    req = req.clone({
+      setHeaders:{
+        access_token: user.token
+      }
+    })
+   }
+    return next.handle(req)
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { HttpInterceptorFn } from '@angular/common/http';
+
+// export const authInterceptor: HttpInterceptorFn = (req, next) => {
+//   return next(req);
+// };
