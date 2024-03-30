dfb08cd9451292a4e7d1b1f21b44dbdc7739d4be
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-30 14:06:20 +0000

    Add user id

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -54,24 +54,31 @@ router.post(
     };
 
     const dbUser = await UserModel.create(newUser);
-    res.send(generateTokenResponse(dbUser))
+    res.send(generateTokenResponse(dbUser));
   })
 );
 
-const generateTokenResponse = (user: any) => {
+const generateTokenResponse = (user: User) => {
   const token = jwt.sign(
     {
+      id: user.id,
       email: user.email,
       isAdmin: user.isAdmin,
     },
-    "SomeRandomText",
+    process.env.JWT_SECRET!,
     {
       expiresIn: "30d",
     }
   );
 
- user.token = token;
- return user;
+  return {
+    id: user.id,
+    email: user.email,
+    name: user.name,
+    address: user.address,
+    isAdmin: user.isAdmin,
+    token: token,
+  };
 };
 
 export default router;
